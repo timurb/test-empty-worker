fc8a0eb964a8d7eb8bca9e9d80483c0686b666b5
Timur Batyrshin <timur.batyrshin@flathost.ru>
2012-10-28 12:57:22 +0400

    script changed a bit to test bundle dependencies working

--- worker.rb
+++ worker.rb
@@ -1,9 +1,10 @@
 #!/usr/bin/env ruby
 
-require 'pp'
+require 'awesome_print'
 
+ap ENV.to_hash
 File.open("/tmp/#{$$}", "w") do |f|
-  f.puts(ENV.pretty_inspect)
+  f.puts(ENV.to_hash.awesome_inspect)
 end
 
 sleep
